f50e128e36f5b08e5a2d69bad72eb1b420a1ee5f
ioBroker Adapter Creator <noreply@iobroker.net>
2025-03-08 21:48:27 +0000

    Initial commit by adapter creator

---
+++ admin/words.js
@@ -0,0 +1,46 @@
+/* eslint no-unused-vars: off */
+/* eslint no-global-assign: off */
+/* global systemDictionary */
+'use strict';
+
+systemDictionary = {
+	'wallboxamo adapter settings': {
+		en: 'Adapter settings for wallboxamo',
+		de: 'Adaptereinstellungen für wallboxamo',
+		ru: 'Настройки адаптера для wallboxamo',
+		pt: 'Configurações do adaptador para wallboxamo',
+		nl: 'Adapterinstellingen voor wallboxamo',
+		fr: "Paramètres d'adaptateur pour wallboxamo",
+		it: "Impostazioni dell'adattatore per wallboxamo",
+		es: 'Ajustes del adaptador para wallboxamo',
+		pl: 'Ustawienia adaptera dla wallboxamo',
+		uk: 'Налаштування адаптера для wallboxamo',
+		'zh-cn': 'wallboxamo的适配器设置',
+	},
+	option1: {
+		en: 'option1',
+		de: 'option1',
+		ru: 'option1',
+		pt: 'option1',
+		nl: 'option1',
+		fr: 'option1',
+		it: 'option1',
+		es: 'option1',
+		pl: 'option1',
+		uk: 'option1',
+		'zh-cn': 'option1',
+	},
+	option2: {
+		en: 'option2',
+		de: 'option2',
+		ru: 'option2',
+		pt: 'option2',
+		nl: 'option2',
+		fr: 'option2',
+		it: 'option2',
+		es: 'option2',
+		pl: 'option2',
+		uk: 'option2',
+		'zh-cn': 'option2',
+	},
+};
